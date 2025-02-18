33b65ab1a3a6c343d8ab686f68c7bdd5b57e084a
Monicka3 <monimska318@gmail.com>
2025-02-18 22:22:51 +0530

    day 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Greetings from "./greetings"; 
+
 function App(){
     return(
         <>
         <h1>Welcome to my coding journey</h1>
+        <Greetings name={"Idiot"}></Greetings>
         </>
     );
 }
---
+++ src/Greetings.jsx
@@ -0,0 +1,9 @@
+function Greetings({name}){
+    return(
+        <>
+        <h2>welcome to Greetings code</h2>
+        <h1>{name}</h1>
+        </>
+    )
+}
+ export default Greetings;
